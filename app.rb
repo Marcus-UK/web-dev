f691d2a818410c6247cf504d6d67962abc08daa0
Marcus Rands <mrands@hotmail.co.uk>
2017-09-11 14:50:37 +0100

    Added Shotgun gem

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   "Hello"
 end
+
+get '/secret' do
+  "Hello Mr Bond. Not so secret now, are we?"
+end
+
+get '/topsecret' do
+  "Shotgun fun"
+end
+
+get '/hello' do
+  "Hi Keith"
+end
